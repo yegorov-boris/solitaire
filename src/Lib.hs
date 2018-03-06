8054d2c916255f48cd83fc09509330486186868c
yegorov-boris <borisyegorov8@gmail.com>
2018-03-06 12:07:36 +0300

    Randomize a deck

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,18 @@
 module Lib
-    ( someFunc
+    ( encode
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.Random
+import System.Random.Shuffle
+
+data Suit = Club | Diamond | Heart | Spade deriving Show
+
+data Card = NormalCard Suit Int | JokerA | JokerB deriving Show
+
+suits = [Club, Diamond, Heart, Spade]
+ranks = [1..13]
+deck = JokerA:JokerB:[NormalCard suit rank | suit <- suits, rank <- ranks]
+deckLength = length deck
+
+encode :: String -> [Card]
+encode message = shuffle' deck deckLength $ mkStdGen deckLength
