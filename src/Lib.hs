c49f1201b75db436a1bfec1c95ac207848605c49
yegorov-boris <borisyegorov8@gmail.com>
2018-03-08 16:55:09 +0300

    Generate a key letter

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,17 +2,95 @@ module Lib
     ( encode
     ) where
 
-import System.Random
-import System.Random.Shuffle
+import System.Random (mkStdGen)
+import System.Random.Shuffle (shuffle')
+import Data.List (takeWhile, dropWhile, elemIndex, last)
+import Data.Maybe (fromJust)
 
-data Suit = Club | Diamond | Heart | Spade deriving Show
+data Suit = Club | Diamond | Heart | Spade deriving (Eq, Show)
 
-data Card = NormalCard Suit Int | JokerA | JokerB deriving Show
+data Card = NormalCard Suit Int | JokerA | JokerB deriving (Eq, Show)
 
+type Hand = [Card]
+
+letters = ['a'..'z']
 suits = [Club, Diamond, Heart, Spade]
 ranks = [1..13]
 deck = JokerA:JokerB:[NormalCard suit rank | suit <- suits, rank <- ranks]
 deckLength = length deck
+shuffledDeck = shuffle' deck deckLength $ mkStdGen deckLength
 
-encode :: String -> [Card]
-encode message = shuffle' deck deckLength $ mkStdGen deckLength
+encode :: String -> Char
+encode message = getKeyLetter shuffledDeck
+
+getKeyLetter :: Hand -> Char
+getKeyLetter deck =
+  let
+    currentDeck = nextState deck
+    card = currentDeck !! (value $ head currentDeck)
+  in
+    if
+      (card == JokerA) || (card == JokerB)
+    then
+      getKeyLetter currentDeck
+    else
+      letters !! ((value card) `mod` 26)
+
+nextState :: Hand -> Hand
+nextState = switchByLast . switchTopBottom . moveJokerB . moveJokerA
+
+moveJokerA :: Hand -> Hand
+moveJokerA deck =
+  let
+    top = takeWhile (/= JokerA) deck
+    bottom = dropWhile (/= JokerA) deck
+  in
+    if
+      (length bottom) == 1
+    then
+      (head top):JokerA:(tail top)
+    else
+      top ++ ((head $ tail bottom):JokerA:(tail $ tail bottom))
+
+moveJokerB :: Hand -> Hand
+moveJokerB deck =
+  let
+    top = takeWhile (/= JokerB) deck
+    bottom = dropWhile (/= JokerB) deck
+  in
+    case bottom of
+      [JokerB] -> (take 2 top) ++ (JokerB:(drop 2 top))
+      [JokerB, bottomLast] -> (head top):JokerB:(tail top) ++ [bottomLast]
+      otherwise -> top ++ (take 2 $ tail bottom) ++ (JokerB:(drop 3 bottom))
+
+switchTopBottom :: Hand -> Hand
+switchTopBottom deck =
+  let
+    indexA = fromJust $ elemIndex JokerA deck
+    indexB = fromJust $ elemIndex JokerB deck
+    indexMin = min indexA indexB
+    indexMax = max indexA indexB
+    top = take indexMin deck
+    bottom = drop (indexMax + 1) deck
+    middle = take (indexMax - indexMin + 1) $ drop indexMin deck
+  in
+    bottom ++ middle ++ top
+
+switchByLast :: Hand -> Hand
+switchByLast deck =
+  let
+    lastCard = last deck
+    step = value lastCard
+    top = take step deck
+    middle = take ((length deck) - step - 1) $ drop step deck
+    bottom = [lastCard]
+  in
+    middle ++ top ++ bottom
+
+value :: Card -> Int
+value JokerA = 53
+value JokerB = 53
+value (NormalCard Club rank) = rank
+value (NormalCard Diamond rank) = rank + 13
+value (NormalCard Heart rank) = rank + 26
+value (NormalCard Spade rank) = rank + 39
